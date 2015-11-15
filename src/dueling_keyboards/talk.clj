59cbaf96a4eea9703d494c0ec6943b9aa0ea1cc3
Chris Ford <christophertford@gmail.com>
2015-11-15 20:52:39 +0000

    Simplify tuning systems.

--- src/dueling_keyboards/talk.clj
+++ src/dueling_keyboards/talk.clj
@@ -12,91 +12,69 @@
 ;;;;;;;;;;;;;;;;;;
 
 (def pythagorean-comma
-  "The difference between an octave constructed out of pure fifths, and one constructed
-  from a pure 2/1 ratio."
+  "The difference between an octave constructed out of pure fifths and 2/1."
   531441/524288)
 
 (def concert-a 440)
-(defn align-concert-a [tuning] (fn [midi] (-> midi tuning (* (/ concert-a (tuning 69))))))
-(defn temper [midi ratios]
+(defn align-concert-a
+  [tuning]
+  (fn [midi] (-> midi (- 69) tuning (* concert-a))))
+
+(defn temper
+  [midi ratios]
   (cond
     (< midi 0) (* 1/2 (temper (+ midi 12) ratios))
     (> midi 11) (* 2 (temper (- midi 12) ratios))
     :otherwise (nth ratios midi)))
 
 (defn tune
-  [root incremental-ratios]
-  (let [geometric-progression (partial reductions * 1)
-        ratios (->>
-                 (geometric-progression incremental-ratios)
-                 (map (fn normalise [ratio] (if (< ratio 2) ratio (normalise (/ ratio 2)))))
-                 sort)
-        tuning (fn [midi] (-> midi (- root) (temper ratios)))]
-    (align-concert-a tuning)))
+  [incremental-ratios]
+  (let [root 69
+        geometric-progression (partial reductions * 1)
+        ratios (->> (geometric-progression incremental-ratios)
+                    (map (fn normalise [ratio] (if (< ratio 2) ratio (normalise (/ ratio 2)))))
+                    sort)]
+    (align-concert-a #(temper % ratios))))
 
-(def equal-temperament
-  "Converts midi to hertz using equal temperament.
-  e.g. (equal 69)"
-  (tune 69 (repeat 11 (java.lang.Math/pow 2 1/12))))
-
-(defn pythagorean
-  "Returns a function that converts midi to hertz using Pythagorean temperament, measuring
-  ratios relative to root. The wolf tone is the fifth from one midi above root.
-  e.g. ((pythagorean 61) 69)"
-  [root]
+(def pythagorean-tuning
+  "Converts midi to hertz, putting the entire Pythagorean comma into one ghastly interval."
   (let [pure 3/2
         wolf (/ pure pythagorean-comma)
-        fifths (mapcat repeat [7 1 3] [pure wolf pure])]
-    (tune root fifths)))
+        fifths [pure pure pure pure pure pure pure wolf pure pure pure]]
+    (tune fifths)))
 
-(defn just [root]
-  "Returns a function that converts midi to hertz using just intonation, measuring ratios
-  relative to root. Specifically, this is a five-limit asymmetric just intonation, with
-  a wolf tone from the major second to the fourth.
-  e.g. ((five-limit-just 61) 69)"
-  (align-concert-a
-    (fn [midi]
-      (temper (- midi root) [1/1 135/128 9/8 6/5 5/4 4/3 45/32 3/2 8/5 27/16 9/5 15/8]))))
-
-(defn meantone
-  "Returns a function that converts midi to hertz using quarter-comma meantone tuning,
-  measuring ratios relative to root. The major third is a pure 5/4 ratio, but there are
-  many wolf tones.
-  e.g. ((meantone 61) 69)"
-  [root]
+(def meantone-temperament
+  "Converts midi to hertz, using a variant of Pythagorean tuning designed to get a consonant major third."
   (let [narrow (java.lang.Math/pow 5 1/4)
         wolf (* narrow 128/125)
-        fifths (mapcat repeat [7 1 3] [narrow wolf narrow])]
-    (tune root fifths)))
+        fifths [narrow narrow narrow narrow narrow narrow narrow wolf narrow narrow narrow]]
+    (tune fifths)))
 
-(defn well
-  "Returns a function that converts midi to hertz using Werckmeister's well-temperament
-  based on 1/4 comma divisions (Werkmeister I). Ratios are relative to root.
-  e.g. ((werckmeister-i 61) 69)"
-  [root]
-  (let [pure 3/2
-        narrow (/ pure (java.lang.Math/pow pythagorean-comma 1/4))
-        fifths [narrow narrow narrow pure pure narrow pure pure pure pure pure]]
-    (tune root fifths)))
+(def equal-temperament
+  "Converts midi to hertz, spreading out the Pythagorean comma evenly across all the intervals."
+  (let [narrow (java.lang.Math/pow 2 7/12)
+        fifths [narrow narrow narrow narrow narrow narrow narrow narrow narrow narrow narrow]]
+    (tune fifths)))
 
-(defn baganda
-  "Converts midi to hertz using 5-tone equal temperament."
+(defn baganda-temperament
+  "Converts midi to hertz, using a five tone version of equal temperament."
   [pitch]
   (/ (* concert-a 4) (reduce * (repeat pitch (java.lang.Math/pow 2 1/5)))))
-
 
 (def baganda-scale
   (->> (phrase (repeat 1/4) (concat (range 18) [17 17 17 17 18 18] [17 17 17 17 18 18] [17 17 17 12 12 12]))
        (canon/canon (comp (canon/simple 1/6) (canon/interval -4)))
        (canon/canon (comp (canon/simple 1/6) (canon/interval -4)))
-       (after -1/3)
-       ;(canon/canon (comp (canon/simple 1/6) (canon/interval -4)))
-       (where :pitch baganda)))
+       (where :pitch baganda-temperament)))
 
 (comment
   (live/play baganda-scale)
   (live/jam (var baganda-scale))
-  )
+)
+
+;;;;;;;;;;;;;;
+;;; Norman ;;;
+;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Analysis by compression ;;;

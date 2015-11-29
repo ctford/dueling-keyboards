fc410ca92aaff31acc0f71fd0fff54b5e242d748
Chris Ford <christophertford@gmail.com>
2015-11-29 18:23:15 +0000

    Ready for performance.

--- src/dueling_keyboards/jam.clj
+++ src/dueling_keyboards/jam.clj
@@ -1,5 +1,5 @@
 (ns dueling-keyboards.jam
-  (:require [overtone.live :refer :all :exclude [stop]]
+  (:require [overtone.live :refer :all :exclude [stop sixth]]
             [leipzig.melody :refer :all]
             [leipzig.canon :as canon]
             [leipzig.scale :as scale]
@@ -29,56 +29,55 @@
                             (chord/root chord/triad 3)
                             chord/triad nil])))
     (tempo (bpm 75))
-    (where :pitch (comp equal-temperament scale/G scale/major))))
-
-(def sec (-> chord/triad (chord/inversion 2) (dissoc :i)))
+    (where :pitch (comp scale/G scale/major))))
 
 (def im-not-worried
-  (let [riff (phrase (cycle [5/2 1/2 1/2 1/2])
-                     [[-2 3.5] -2 1 0
-                      [-2 3] -2 1 0
-                      [-3 2] -3 1 0
-                      [-3 1] -3 1 0])
+  (let [riff (->> (phrase (cycle [5/2 1/2 1/2 1/2])
+                          [[-2 3.5] -2 1 0
+                           [-2 3] -2 1 0
+                           [-3 2] -3 1 0
+                           [-3 1] -3 1 0])
+                  (times 4))
         bass (->> (phrase (repeat 1/2)
                           (mapcat
                             repeat
                             (cycle [6 1 1])
-                            [1 1 2 3 2 1 0 -1 -2 -3 1 -3]))
-                  (where :pitch scale/lower))
-        chorus (->> (with bass riff)
-                    (times 2))
-        plain (->> (phrase [16] [[-3 -10]])
-                   (with (phrase (repeat 64 1/4)
-                                 (mapcat #(take 16
-                                                (cycle [%2 %1 11 %1]))
-                                         [6 7.5 7 6]
-                                         [8 9 9 8])))
-                   (with (phrase (repeat 4) [8 7.5 7 6.5])))
-        chords (phrase (repeat 4)
-                          [(-> sec (chord/root 4))
-                           (-> sec (chord/root 5)
-                               (update-in [:iii] + 0.5))
-                           (-> sec (chord/root 5))
-                           (-> sec (chord/root 4))])
-        scene (phrase (repeat 32 1/2) (repeat [-3 -10]))
-        vary (with scene chords)
-        tail (->> (phrase (repeat 8 1/2)
-                          (cycle [-3 -3 -3 -3 -3 -3 1 -3]))
-                  (where :pitch scale/lower)
-                  (with (phrase [5/2 1/2 1/2 1/2]
-                                [[-3 1] -2 1 0])))
-        harmony (phrase [8 6 1 1 8 4 2 1 1]
-                        [12 11 12 13 12 14 13 12 11])]
-    (->> (times 2 bass)
-         (then (times 2 chorus))
-         (then (times 3 tail))
-         (then (times 4 vary))
-         (then (times 2 (with chorus harmony)))
-         (then (times 3 tail))
-         (then (times 4 plain))
-         (tempo (bpm 150))
-         (where :pitch (comp scale/C scale/major))
-         (where :pitch equal-temperament))))
+                            [-6 -6 -5 -4 -5 -6 -7 -8 -9 -10 -6 -10]))
+                  (times 4))
+        arpeggios (->> (phrase
+                         (repeat 64 1/4)
+                         (mapcat #(take 16 (cycle [%2 %1 11 %1]))
+                                 [6 7.5 7 6]
+                                 [8 9 9 8]))
+                       (times 4))
+        harmonies (->> (phrase
+                         (cycle [10/4 1 1/4 1/4 4])
+                         [[1 6] 4 1 1.5 [2 7.5] [2 7] 5 3 3.5 [4 6]])
+                       (times 4))
+        pulse (phrase (repeat 64 1/2) (repeat -3))
+        postscript (->> (phrase (repeat 8 1/2)
+                                (cycle [-10 -10 -10 -10 -10 -10 -6 -10]))
+                        (with (phrase [5/2 1/2 1/2 1/2]
+                                      [[-3 1] -2 1 0]))
+                        (times 3))
+        extra (->> (phrase [8 6 1 1 8 4 2 1 1]
+                           [12 11 12 13 12 14 13 12 11])
+                   (times 2))]
+    (->>
+      ;(with
+      bass
+      ;riff
+      ;extra
+      ;)
+      (then postscript)
+      (then (with
+              pulse
+              harmonies
+              ;arpeggios
+              ))
+      (then postscript)
+      (tempo (bpm 150))
+      (where :pitch (comp scale/C scale/major)))))
 
 (definst over-it [freq 440 dur 1.0 vol 0.5]
   (-> (sin-osc freq)
@@ -94,5 +93,5 @@
       (* vol)))
 
 (defmethod live/play-note :default
-  [{hz :pitch seconds :duration}]
-  (when hz (over-it hz seconds)))
+  [{midi :pitch seconds :duration}]
+  (some-> midi equal-temperament (over-it seconds)))
